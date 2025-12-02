a1c24aab1f33cd73757ba4dc7a575ae7a49f315e
Julio Oliveira <antoniojuliodeo@gmail.com>
2025-12-02 18:48:04 -0300

    feat(day_1): part_1, part_2, input and input_example

---
+++ day_1/part_2/day_1_part2.py
@@ -0,0 +1,98 @@
+from enum import Enum
+from dataclasses import dataclass
+
+
+class Direction(str, Enum):
+    L = "L"
+    R = "R"
+
+
+@dataclass
+class Rotation:
+    direction: Direction
+    clicks: int
+
+    @staticmethod
+    def create_rotation(raw_rotation: str) -> "Rotation":
+        if not isinstance(raw_rotation, str):
+            raise ValueError(f"{raw_rotation} is not str!")
+
+        if len(raw_rotation) < 2:
+            raise ValueError(f"{raw_rotation} legth is too short!")
+
+        if len(raw_rotation) > 4:
+            raise ValueError(f"{raw_rotation} legth is too long!")
+
+        if raw_rotation[0] not in list(Direction):
+            raise ValueError(f"{raw_rotation} direction can only be R or L!")
+
+        direction = Direction(raw_rotation[0])
+        raw_clicks = raw_rotation[1:]
+
+        if not str.isdigit(raw_clicks):
+            raise ValueError(f"{raw_rotation} clicks should be a int!")
+
+        clicks = int(raw_clicks)
+
+        return Rotation(direction=direction, clicks=clicks)
+
+
+@dataclass
+class Safe:
+    dial_point: int = 50
+    crossed: bool = False
+
+    def turn_dial(self, rotation: Rotation) -> "Safe":
+        operator = 1
+        if rotation.direction == Direction.L:
+            operator = -1
+
+        raw_new_dial_point: int = self.dial_point + (operator * rotation.clicks)
+        crossed: bool = (raw_new_dial_point < 0) or (raw_new_dial_point > 99)
+        new_dial_point: int = raw_new_dial_point % 100
+        return Safe(dial_point=new_dial_point, crossed=crossed)
+
+
+@dataclass
+class PasswordDiscoverState:
+    safe_state: Safe = Safe()
+    how_many_times_dial_zero: int = 0
+
+    def execute_rotation(self, rotation: Rotation) -> "PasswordDiscoverState":
+        circles: int = rotation.clicks // 100
+        simplified_rotation = Rotation(
+            direction=rotation.direction, clicks=rotation.clicks % 100
+        )
+        new_safe_state: Safe = self.safe_state.turn_dial(rotation=simplified_rotation)
+        new_how_many_times_dial_zero: int = self.how_many_times_dial_zero
+
+        if self.safe_state.dial_point == 0:
+            new_how_many_times_dial_zero += circles
+
+        elif new_safe_state.dial_point == 0:
+            new_how_many_times_dial_zero += circles + 1
+
+        else:
+            new_how_many_times_dial_zero += circles + int(new_safe_state.crossed)
+
+        return PasswordDiscoverState(
+            safe_state=new_safe_state,
+            how_many_times_dial_zero=new_how_many_times_dial_zero,
+        )
+
+
+if __name__ == "__main__":
+    password_discover_state = PasswordDiscoverState()
+
+    while True:
+        try:
+            rotation: Rotation = Rotation.create_rotation(raw_rotation=input().strip())
+            password_discover_state: PasswordDiscoverState = (
+                password_discover_state.execute_rotation(rotation=rotation)
+            )
+        except EOFError:
+            break
+        except ValueError as err:
+            print(err)
+
+    print(password_discover_state)
